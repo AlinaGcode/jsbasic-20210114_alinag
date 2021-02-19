66179646b1114fb1f9767ec16e96fd88b98af1a3
Alina Gugkaeva <alinakulish@gmail.com>
2021-02-19 14:13:33 +0100

    новый коммит task 3 and 4

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,17 @@
 function toggleText() {
-  // ваш код...
+  let elem = document.querySelector(".toggle-text-button");
+  var el = document.getElementById("text");
+  
+  function show() {
+    
+    if (el.hidden == true) {
+      return el.hidden = false;
+    } else {
+      return el.hidden = true;
+    }
+  };
+  elem.addEventListener("click", show);
 }
+
+
+
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,41 @@
 function initCarousel() {
-  // ваш код...
+  
+  let width = 988;
+  let count = 1;
+  
+  let slideList = document.querySelector(".carousel_inner");
+  let slide = document.getElementsByClassName(".carousel_slide");
+  slide.length 
+  
+  let position = 0;
+  let rightClickButton = document.querySelector('.carousel__arrow_right');
+  
+  let leftClickButton = document.querySelector(".carousel__arrow_left");
+  
+  rightClickButton.onclick = function () {
+    position += width * count;
+    let slides = document.querySelector(".carousel__inner");
+    slides.style.transform = "translateX(-" + position + "px)"; 
+    
+  };
+  
+  
+  leftClickButton.onclick = function () {
+    position -= width * count;
+    let slides = document.querySelector(".carousel__inner");
+    slides.style.transform = "translateX(-" + position + "px)"; 
+    
+  }
+  
 }
+/*  
+rightClickButton.classList.toggle("disabled", slideIndex === 0);
+leftClickButton.classList.toggle("disabled", slideIndex === --slide.length);
+
+for (let i = 0; i <= slide.length; i++) {
+  if (i = 3) {
+    rightClickButton.style.display = "none"
+  } else {
+    rightClickButton.style.display = " "
+  }
+  */
